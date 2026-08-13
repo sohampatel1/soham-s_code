1cb578a71e3924d8c51cbe8f0861579505f88b6c
sohampatel1 <sohamvora.140448@marwadiuniversity.ac.in>
2026-08-13 23:01:26 +0530

    Time: 6 ms (8.77%), Space: 44.5 MB (95.02%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-    List<Integer>a=new ArrayList();
-    for(int i=0;i<nums2.length;i++)
-    {a.add(nums2[i]);
-    }
-    for(int i=0;i<nums1.length;i++)
-    {int in=a.indexOf(nums1[i]);
-    if(in==a.size()-1) nums1[i]=-1;
-    else
-    {for(int j=in;j<a.size();j++)
-    {if(nums1[i]<a.get(j)) 
-    {nums1[i]=a.get(j);
-    break;}
-    }
-    if(nums1[i]==a.get(in))nums1[i]=-1;
-    }
-    }
-    return nums1;
+         int n=nums1.length;
+         int m=nums2.length;
+         int[] ans=new int[n];
+         for(int i=0;i<n;i++){
+             for(int j=0;j<m;j++){
+                 if(nums1[i]==nums2[j]){
+                      ans[i] = -1;
+                     for (int k = j + 1; k < m; k++) {
+                         if (nums2[k] > nums2[j]) {
+                             ans[i] = nums2[k];
+                             break;
+                         }
+                     }
+                }
+             }
+         }
+         return ans; 
     }
 }
